3a6ed31a0a77fd24fd834f802c2318578ca7b41d
NGuyenVAn21 <dinhvanparkour@gmail.com>
2026-04-06 17:18:57 +0700

    B2: Tao Model SinhVien (Code First)

---
+++ BT_Code_First/Models/SinhVien.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BT_Code_First.Models
+{
+    public class SinhVien
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Ten { get; set; }
+
+        public int Tuoi { get; set; }
+
+        public string DiaChi { get; set; }
+    }
+}
