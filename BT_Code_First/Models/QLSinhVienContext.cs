424265685cef494f50a80a6abe9e38c7b868da0f
NGuyenVAn21 <dinhvanparkour@gmail.com>
2026-04-06 17:24:37 +0700

    B3: Code DbContext QLSinhVienContext

--- BT_Code_First/Models/QLSinhVienContext.cs
+++ BT_Code_First/Models/QLSinhVienContext.cs
@@ -1,6 +1,14 @@
-﻿namespace BT_Code_First.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BT_Code_First.Models
 {
-    public class QLSinhVienContext
+    public class QLSinhVienContext : DbContext
     {
+        public QLSinhVienContext(DbContextOptions<QLSinhVienContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SinhVien> SinhViens { get; set; }
     }
 }
